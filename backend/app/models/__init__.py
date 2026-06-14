c0c0df8c0f7981c94665716fbb25b70969533c27
AfzalSurti <surtishabana35@gmail.com>
2026-06-14 18:42:45 +0530

    chore: register all database models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.employee import Employee
+from app.models.project import Project, EmployeeProject
+from app.models.attendance import Attendance
+from app.models.holiday import Holiday
